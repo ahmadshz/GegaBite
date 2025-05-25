3ec1f182a54470366f4fe5d2df1a2222747eed0c
Ahmad Shmeit <shmeitahmad3@gmail.com>
2025-05-25 17:05:59 +0300

    update

--- src/components/Dashboard/SideBar.jsx
+++ src/components/Dashboard/SideBar.jsx
@@ -5,7 +5,7 @@ import { TbCategory2, TbCategoryPlus } from 'react-icons/tb';
 import { NavLink, useNavigate } from 'react-router-dom';
 import apiClient from '../../Api/ApiClient';
 
-const SideBar = ({ isOpen }) => {
+const SideBar = ({ isOpen, toggleClose }) => {
   const navigate = useNavigate();
 
   const navLink = [
@@ -39,8 +39,8 @@ const SideBar = ({ isOpen }) => {
   };
 
   return (
-    <div className={` lg:fixed bottom-0 ${isOpen ? "fixed z-10 w-60 xl:w-80 px-3 lg:px-8" : "w-0 border-r-0 lg:border-r-2 lg:w-20 lg:px-4"} 
-      h-[calc(100vh-70px)] lg:h-[calc(100%-80px)] border-r-2 border-[#282828] dark:border-gray-600 
+    <div className={`lg:fixed bottom-0 ${isOpen ? "fixed z-30 w-60 xl:w-80 px-3 lg:px-8" : " w-0 border-r-0 lg:border-r-2 lg:w-20 lg:px-4"} 
+      h-[calc(100vh-70px)] xl:h-[calc(100%-80px)] border-r-2 border-[#282828] dark:border-gray-600 
       flex flex-col items-center justify-between gap-3 py-4 dark:bg-[#282828] duration-300 bg-white`}>
 
       <div className='flex flex-col w-full gap-3'>
@@ -48,11 +48,12 @@ const SideBar = ({ isOpen }) => {
           <NavLink
             to={item.link}
             key={index}
+            onClick={() => window.innerWidth < 1024 && toggleClose()}
             className={({ isActive }) =>
               `text-xl font-semibold w-full rounded-md flex items-center gap-2 dark:text-white
-              ${isOpen ? "p-3" : " hidden lg:flex justify-center p-2 "}
-              ${isActive  ? " lg:bg-gray-200 lg:dark:bg-[#515151]" : ""}
-              ${isActive && isOpen  ? " bg-gray-200 dark:bg-[#515151]" : ""}`
+              ${isOpen ? "p-3" : "hidden lg:flex lg:justify-center lg:p-2"}
+              ${isActive ? "lg:bg-gray-200 lg:dark:bg-[#515151]" : ""}
+              ${isActive && isOpen ? "bg-gray-200 dark:bg-[#515151]" : ""}`
             }
           >
             {item.icon}
@@ -65,7 +66,7 @@ const SideBar = ({ isOpen }) => {
 
       <button
         onClick={handleLogout}
-        className={`${isOpen ? "" : "hidden lg:block"} font-semibold w-full rounded-md flex items-center gap-2 bg-red-600 text-white p-3`}
+        className={`${isOpen ? "" : "flex lg:justify-center"} font-semibold w-full rounded-md flex items-center gap-2 bg-red-600 text-white p-3`}
       >
         <BiLogOut size={24} />
         <span className={`${isOpen ? "block" : "hidden"} text-nowrap`}>
--- src/components/Dashboard/TopBar.jsx
+++ src/components/Dashboard/TopBar.jsx
@@ -1,23 +1,20 @@
 import { LuPanelLeftClose } from "react-icons/lu";
 import { LuPanelRightClose } from "react-icons/lu";
 
-
 const TopBar = ({ toggleClose, isOpen }) => {
-
-
-    return (
-        <div className=' lg:fixed z-10  h-[70px] xl:h-20 w-full border-b-2 border-[#282828] dark:border-gray-600 bg-white dark:bg-[#282828] '>
-            <div className= ' px-3 lg:px-8 h-full flex items-center gap-2 lg:gap-5'>
-                {
-                    isOpen ?
-                        <LuPanelLeftClose onClick={toggleClose} className='text-3xl xl:text-4xl  dark:text-white' />
-                        :
-                        <LuPanelRightClose onClick={toggleClose} className='text-3xl xl:text-4xl  dark:text-white ' />
-                }
-                <h1 className='  text-2xl xl:text-4xl font-bold  dark:text-white'>Gega Bite</h1>
-            </div>
-        </div>
-    )
+  return (
+    <div className='fixed z-10 h-[70px] xl:h-20 w-full border-b-2 border-[#282828] dark:border-gray-600 bg-white dark:bg-[#282828]'>
+      <div className='px-3 lg:px-8 h-full flex items-center gap-2 lg:gap-5'>
+        {
+          isOpen ?
+            <LuPanelLeftClose onClick={toggleClose} className='text-3xl xl:text-4xl dark:text-white' />
+            :
+            <LuPanelRightClose onClick={toggleClose} className='text-3xl xl:text-4xl dark:text-white' />
+        }
+        <h1 className='text-2xl xl:text-4xl font-bold dark:text-white'>Gega Bite</h1>
+      </div>
+    </div>
+  )
 }
 
 export default TopBar
