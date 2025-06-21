26116235dfd3a094ba008ff2ef60be99708c6d5b
Ahmad Shmeit <shmeitahmad3@gmail.com>
2025-06-21 15:27:52 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import RequireAuth from './Auth/RequireAuth';
 import RequireBack from './Auth/RequireBack';
 import SingleProduct from './Pages/Website/SingleProduct';
 import BtnToTop from './components/Ui/BtnToTop';
+import img from './assets/logo.png'
 
 const App = () => {
   const location = useLocation();
@@ -31,23 +32,22 @@ const App = () => {
 
   const isMainLocation = location.pathname === '/';
 
-  const showBtnToTop = !location.pathname.startsWith('/dashboard') && location.pathname !== '/login' &&   !location.pathname.startsWith('/singleproduct');
-;
+  const showBtnToTop = !location.pathname.startsWith('/dashboard') && location.pathname !== '/login' && !location.pathname.startsWith('/singleproduct');
+  ;
 
   if (showPreload) return (
-    <div className='fixed inset-0 bg-[#FEC30D] dark:bg-black z-50 flex flex-col items-center justify-center text-center px-4'>
+    <div className='fixed inset-0 bg-[#FEC30D]  z-50 flex flex-col items-center justify-center text-center px-4'>
+      <div className='bg-white p-4 rounded-full w-32 h-32 flex items-center justify-center'>
+        <img src={img} className=' w-[85%]' alt='' />
+
+      </div>
       {
         isMainLocation &&
-        <h1 className='text-3xl font-bold text-black dark:text-white mb-4'>
+        <h1 className=' text-2xl lg:text-3xl font-bold text-black  mt-4'>
           Welcome to Gega Bite
         </h1>
       }
-      <div>
-        <svg className="animate-spin -ml-1 mr-3 h-8 w-8 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-          <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-          <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-        </svg>
-      </div>
+
     </div>
   );
 
